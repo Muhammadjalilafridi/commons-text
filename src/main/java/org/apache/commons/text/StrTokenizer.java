ce0a106d08a8fa29a4158c55547ad254630ac437
Muhammadjalilafridi <129916781+Muhammadjalilafridi@users.noreply.github.com>
2024-03-08 21:37:57 +0100

    Update StrTokenizer.java

--- src/main/java/org/apache/commons/text/StrTokenizer.java
+++ src/main/java/org/apache/commons/text/StrTokenizer.java
@@ -450,14 +450,7 @@ public class StrTokenizer implements ListIterator<String>, Cloneable {
         try {
             return cloneReset();
         } catch (final CloneNotSupportedException ex) {
-           return createNonNullClone();
-        }
-    }
- private Object createNonNullClone() {
-        try {
-            return super.clone();
-        } catch (CloneNotSupportedException e) {
-            return new Object();
+           return null;
         }
     }
     /**
