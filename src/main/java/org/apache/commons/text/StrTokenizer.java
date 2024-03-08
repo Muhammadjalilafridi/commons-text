db53c8c617985a84fa3d02631ff959b0f824654e
Muhammadjalilafridi <129916781+Muhammadjalilafridi@users.noreply.github.com>
2024-03-08 21:02:42 +0100

    Update StrTokenizer.java

--- src/main/java/org/apache/commons/text/StrTokenizer.java
+++ src/main/java/org/apache/commons/text/StrTokenizer.java
@@ -450,10 +450,16 @@ public class StrTokenizer implements ListIterator<String>, Cloneable {
         try {
             return cloneReset();
         } catch (final CloneNotSupportedException ex) {
-            return "";
+           return createNonNullClone();
         }
     }
-
+ private Object createNonNullClone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            return new Object();
+        }
+    }
     /**
      * Creates a new instance of this Tokenizer. The new instance is reset so that
      * it will be at the start of the token list.
